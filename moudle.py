227e4dc3301e24cd7ca8cbd04bd047d87451c3de
gerivii <wzt1392984327@outlook.com>
2021-06-05 20:56:56 +0800

    更新了一些错误

--- moudle.py
+++ moudle.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as f
 
@@ -27,7 +28,6 @@ class ResBlock(nn.Module):
         out = self.left(x)
         out += self.shortcut(x)
         out = f.relu(out)
-
         return out
 
 
@@ -36,15 +36,16 @@ class ResNet(nn.Module):
         super(ResNet, self).__init__()
         self.in_channel = 64
         self.conv1 = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=(7, 7), stride=(2, 2), padding=(1, 1), bias=False),
+            nn.Conv2d(3, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False),
             nn.BatchNorm2d(64),
-            nn.ReLU()
+            nn.ReLU(True)
         )
-        nn.MaxPool2d((3, 3), (2, 2))
+        self.max_pool = nn.MaxPool2d((3, 3), (2, 2))
         self.layer1 = self.make_layer(res_block, 64, 2, stride=1)
         self.layer2 = self.make_layer(res_block, 128, 2, stride=1)
         self.layer3 = self.make_layer(res_block, 256, 2, stride=1)
         self.layer4 = self.make_layer(res_block, 512, 2, stride=1)
+        self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(512, num_classes)
 
     def make_layer(self, block, channels, num_blocks, stride):
@@ -57,11 +58,12 @@ class ResNet(nn.Module):
 
     def forward(self, x):
         out = self.conv1(x)
+        out = self.max_pool(out)
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
-        out = f.avg_pool2d(out, 4)
-        out = out.view(out.size(0), -1)
+        out = self.avg_pool(out)
+        out = torch.flatten(out, 1)
         out = self.fc(out)
         return out
